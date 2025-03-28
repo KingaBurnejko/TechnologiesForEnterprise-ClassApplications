9c9278a73fb985931d07da5bdb6c8531ea5d0f01
kinga.burnejko <burnejko.kinga@gmial.com>
2025-03-29 00:44:51 +0100

    Upload: lab6 - ASP.NET MVC project

---
+++ MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime TimeStamp { get; set; }
+
+        [Required]
+        [StringLength(140, MinimumLength = 5)]
+        public string Text { get; set; } = string.Empty;
+
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+    }
+}
